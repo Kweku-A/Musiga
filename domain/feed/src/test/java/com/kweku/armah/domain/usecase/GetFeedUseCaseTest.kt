8eac0c34e123ff6de5a9d29b1cc148dd7582efd1
Kweku <thunderson3@gmail.com>
2022-09-26 20:40:51 +0200

    Rename to GetFeedUseCaseTest

--- domain/feed/src/test/java/com/kweku/armah/domain/usecase/GetFeedUseCaseTest.kt
+++ domain/feed/src/test/java/com/kweku/armah/domain/usecase/GetFeedUseCaseTest.kt
@@ -6,51 +6,37 @@ import com.kweku.armah.domain.repository.fake.FakeFeedRepository
 import com.kweku.armah.networkresult.ApiErrorType
 import com.kweku.armah.networkresult.ApiResult.ApiError
 import com.kweku.armah.networkresult.ApiResult.ApiSuccess
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
-class GetMusicFeedUseCaseTest {
+class GetFeedUseCaseTest {
     private lateinit var repository: FakeFeedRepository
-    private lateinit var sut: GetMusicFeedUseCase
+    private lateinit var sut: GetFeedUseCase
 
     @Before
     fun init() {
         repository = FakeFeedRepository()
-        sut = GetMusicFeedUseCase(repository = repository)
+        sut = GetFeedUseCase(repository = repository)
     }
 
     @Test
     fun should_return_list_of_sessions() {
-        val expectedApiResult = ApiSuccess(fakeSessions)
-        val actualApiResult = runBlocking { sut() }
-        assertEquals(expectedApiResult, actualApiResult)
-
         val expected = fakeSessions
-        val actual = (actualApiResult as ApiSuccess).data
+        repository.response = ApiSuccess(expected)
+        val actual = runBlocking { sut().first() }
         assertEquals(expected, actual)
     }
 
     @Test
-    fun should_return_not_found_type_on_request_failure() {
+    fun should_return_empty_list_request_failure() {
         val expectedResult = ApiError<List<Session>>(ApiErrorType.NOT_FOUND)
         repository.response = expectedResult
-        val expected = expectedResult.type
+        val expected = emptyList<Session>()
 
-        val result = runBlocking { sut() }
-        val actual = (result as ApiError).type
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun should_return_exception_on_network_exception() {
-        val expectedResult = ApiError<List<Session>>(ApiErrorType.EXCEPTION)
-        repository.response = expectedResult
-        val expected = expectedResult.type
-
-        val result = runBlocking { sut() }
-        val actual = (result as ApiError).type
+        val actual = runBlocking { sut().first()  }
         assertEquals(expected, actual)
     }
 }
