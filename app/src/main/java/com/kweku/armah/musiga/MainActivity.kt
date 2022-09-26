beff66d8c3d6f4ede2872551e7379cde779f66b2
Kweku <thunderson3@gmail.com>
2022-09-26 20:47:11 +0200

    Update MainActivity.kt

--- app/src/main/java/com/kweku/armah/musiga/MainActivity.kt
+++ app/src/main/java/com/kweku/armah/musiga/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.kweku.armah.musiga
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,18 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
-import com.kweku.armah.domain.usecase.GetMusicFeedUseCase
+import com.kweku.armah.domain.usecase.GetFeedUseCase
 import com.kweku.armah.musiga.ui.theme.MusigaTheme
-import com.kweku.armah.networkresult.ApiResult
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @Inject
-    lateinit var getMusicFeedUseCase: GetMusicFeedUseCase
+    lateinit var getFeedUseCase: GetFeedUseCase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,18 +34,7 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
-    override fun onResume() {
-        super.onResume()
-
-        lifecycleScope.launch {
-           val value= getMusicFeedUseCase()
-            Log.d("TAG",(value as ApiResult.ApiSuccess).data.toString())
-        }
-    }
 }
-
-
 
 @Composable
 fun Greeting(name: String) {
