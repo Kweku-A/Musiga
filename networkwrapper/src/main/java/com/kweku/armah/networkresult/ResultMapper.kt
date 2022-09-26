9f7a523a983b7bc7ef73d6d33e682353a8e2802b
Kweku <thunderson3@gmail.com>
2022-09-26 20:36:26 +0200

    Format network files

--- networkwrapper/src/main/java/com/kweku/armah/networkresult/ResultMapper.kt
+++ networkwrapper/src/main/java/com/kweku/armah/networkresult/ResultMapper.kt
@@ -1,6 +1,5 @@
 package com.kweku.armah.networkresult
 
-
 fun <T, C> ApiResult<T>.mapResult(mapper: (T) -> C): ApiResult<C> {
     return when (this) {
         is ApiResult.ApiError -> ApiResult.ApiError(type)
