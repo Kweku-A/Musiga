d562f281cd196b365919a7aefd3107947baba833
Kweku <thunderson3@gmail.com>
2022-09-26 20:37:54 +0200

    Update FeedRepository.kt

--- domain/feed/src/main/java/com/kweku/armah/domain/repository/FeedRepository.kt
+++ domain/feed/src/main/java/com/kweku/armah/domain/repository/FeedRepository.kt
@@ -2,8 +2,12 @@ package com.kweku.armah.domain.repository
 
 import com.kweku.armah.domain.model.Session
 import com.kweku.armah.networkresult.ApiResult
+import kotlinx.coroutines.flow.StateFlow
 
 interface FeedRepository {
-    suspend fun getFeed(): ApiResult<List<Session>>
-    suspend fun getSearchedFeed(searchParam: String): ApiResult<List<Session>>
+    suspend fun getFeedDto(): ApiResult<Unit>
+
+    suspend fun getLocalFeed(limit:Int,offset:Int): StateFlow<List<Session>>
+
+    suspend fun deleteLocalFeed(): Boolean
 }
