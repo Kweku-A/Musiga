a442c49de3241951dc5a9f1c723b2e0fe5908fb5
Kweku <thunderson3@gmail.com>
2022-09-26 20:47:53 +0200

    Create CoroutineModule.kt

---
+++ app/src/main/java/com/kweku/armah/musiga/di/CoroutineModule.kt
@@ -0,0 +1,20 @@
+package com.kweku.armah.musiga.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutineModule {
+
+    @Provides
+    @Singleton
+    fun providesIOCoroutine(): CoroutineScope {
+        return CoroutineScope(Dispatchers.IO)
+    }
+}
