d4ddf269dfbd5ab32163acadd0e9a3613f51fe05
Kweku <thunderson3@gmail.com>
2022-09-26 20:37:14 +0200

    Update FakeFeedRepository.kt

--- domain/feed/src/main/java/com/kweku/armah/domain/repository/fake/FakeFeedRepository.kt
+++ domain/feed/src/main/java/com/kweku/armah/domain/repository/fake/FakeFeedRepository.kt
@@ -4,16 +4,40 @@ import com.kweku.armah.domain.model.Session
 import com.kweku.armah.domain.model.fake.fakeSessions
 import com.kweku.armah.domain.repository.FeedRepository
 import com.kweku.armah.networkresult.ApiResult
+import com.kweku.armah.networkresult.ApiResult.ApiError
 import com.kweku.armah.networkresult.ApiResult.ApiSuccess
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 
 class FakeFeedRepository : FeedRepository {
 
-    var response:ApiResult<List<Session>> = ApiSuccess(fakeSessions)
-    override suspend fun getFeed(): ApiResult<List<Session>> {
-        return response
+    var response: ApiResult<List<Session>> = ApiSuccess(fakeSessions)
+    private var _sessionsFlow: MutableStateFlow<List<Session>> = MutableStateFlow(listOf())
+
+    private val sessionsFlow: StateFlow<List<Session>> = _sessionsFlow
+
+    override suspend fun getFeedDto(): ApiResult<Unit> {
+        val result = when (response) {
+            is ApiSuccess -> {
+                _sessionsFlow.value = (response as ApiSuccess<List<Session>>).data
+                ApiSuccess(Unit)
+            }
+
+            is ApiError -> {
+                val errorType = (response as ApiError<List<Session>>).type
+                ApiError(errorType)
+            }
+        }
+        return result
     }
 
-    override suspend fun getSearchedFeed(searchParam: String): ApiResult<List<Session>> {
-        return response
+    override suspend fun getLocalFeed(): StateFlow<List<Session>> {
+        return sessionsFlow
+    }
+
+    override suspend fun deleteLocalFeed(): Boolean {
+       _sessionsFlow.value = emptyList()
+        return sessionsFlow.first().isEmpty()
     }
 }
