85ad1ed9f9e0dd62a9ef5b43469ced4d820ca828
Kweku <thunderson3@gmail.com>
2022-09-26 20:45:50 +0200

    Update AppModule with use cases

--- app/src/main/java/com/kweku/armah/musiga/di/AppModule.kt
+++ app/src/main/java/com/kweku/armah/musiga/di/AppModule.kt
@@ -1,7 +1,12 @@
 package com.kweku.armah.musiga.di
 
 import com.kweku.armah.domain.repository.FeedRepository
-import com.kweku.armah.domain.usecase.GetMusicFeedUseCase
+import com.kweku.armah.domain.repository.SearchFeedRepository
+import com.kweku.armah.domain.usecase.DeleteLocalFeedUseCase
+import com.kweku.armah.domain.usecase.DeleteLocalSearchFeedUsesCase
+import com.kweku.armah.domain.usecase.FeedUseCases
+import com.kweku.armah.domain.usecase.GetFeedUseCase
+import com.kweku.armah.domain.usecase.SearchFeedUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,7 +19,40 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun providesGetFeedUseCase(repository: FeedRepository): GetMusicFeedUseCase {
-        return GetMusicFeedUseCase(repository)
+    fun providesGetFeedUseCase(repository: FeedRepository): GetFeedUseCase {
+        return GetFeedUseCase(repository)
     }
+
+    @Singleton
+    @Provides
+    fun providesDeleteLocalFeedUseCase(repository: FeedRepository): DeleteLocalFeedUseCase {
+        return DeleteLocalFeedUseCase(repository)
+    }
+
+    @Singleton
+    @Provides
+    fun providesSearchFeedUseCase(repository: SearchFeedRepository): SearchFeedUseCase {
+        return SearchFeedUseCase(repository)
+    }
+
+    @Singleton
+    @Provides
+    fun providesDeleteLocalSearchFeedUsesCase(repository: SearchFeedRepository): DeleteLocalSearchFeedUsesCase {
+        return DeleteLocalSearchFeedUsesCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFeedUseCases(
+        getFeedUseCase: GetFeedUseCase,
+        searchFeedUseCase: SearchFeedUseCase,
+        deleteLocalFeedUseCase: DeleteLocalFeedUseCase,
+        deleteLocalSearchFeedUsesCase: DeleteLocalSearchFeedUsesCase
+    ) = FeedUseCases(
+        searchFeedUseCase,
+        getFeedUseCase,
+        deleteLocalSearchFeedUsesCase,
+        deleteLocalFeedUseCase
+    )
+
 }
