72b541ce3d921ca769d56fb7f33b6f2df066dd28
Kweku <thunderson3@gmail.com>
2022-09-29 16:01:12 +0200

    Add UI model

---
+++ presentation/ui/src/main/java/com/kweku/armah/ui/model/SessionUi.kt
@@ -0,0 +1,10 @@
+package com.kweku.armah.ui.model
+
+data class SessionUi(
+    val id:Long,
+    val artworkUrl: String,
+    val title: String,
+    val genres: String,
+    val listenerCount: Int,
+    val name: String
+)
