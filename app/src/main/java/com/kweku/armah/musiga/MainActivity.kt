0c2c1679faef8eba0b80bde46d27203727288880
Kweku <thunderson3@gmail.com>
2022-09-29 16:03:49 +0200

    Update MainActivity.kt

--- app/src/main/java/com/kweku/armah/musiga/MainActivity.kt
+++ app/src/main/java/com/kweku/armah/musiga/MainActivity.kt
@@ -3,11 +3,7 @@ package com.kweku.armah.musiga
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kweku.armah.musiga.navigation.AppNavigation
 import com.kweku.armah.musiga.ui.theme.MusigaTheme
@@ -18,15 +14,10 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             MusigaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
                     AppNavigation()
-                }
             }
         }
     }
