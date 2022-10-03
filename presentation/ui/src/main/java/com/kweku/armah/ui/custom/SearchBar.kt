ff89ae459248afb70a67ad281b9a8339998341ca
Kweku <thunderson3@gmail.com>
2022-10-03 22:39:18 +0200

    Add custom textfield with search

--- presentation/ui/src/main/java/com/kweku/armah/ui/custom/SearchBar.kt
+++ presentation/ui/src/main/java/com/kweku/armah/ui/custom/SearchBar.kt
@@ -1,84 +1,114 @@
 package com.kweku.armah.ui.custom
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.kweku.armah.ui.R
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 internal fun SearchBar(
     searchTextProvider: () -> String,
     onSearchTextChanged: (String) -> Unit,
     modifier: Modifier = Modifier,
-    placeholderText: String = "Search",
+    placeholderText: String,
     isSearchingProvider: () -> Boolean,
+) {
+    Box(modifier = modifier.clip(shape = RoundedCornerShape(20.dp))) {
+        Row(
+            modifier = Modifier
+                .background(color = Color.DarkGray)
+                .padding(10.dp)
+        ) {
+            when (isSearchingProvider()) {
+                true -> {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(25.dp)
+                    )
+                }
+
+                false -> {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = stringResource(R.string.search_field),
+                        tint = Color.Gray
+                    )
+                }
+            }
+
+            CustomTextField(
+                placeholderText = placeholderText,
+                searchTextProvider = searchTextProvider,
+                onSearchTextChanged = onSearchTextChanged
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+private fun CustomTextField(
+    placeholderText: String,
+    searchTextProvider: () -> String,
+    onSearchTextChanged: (String) -> Unit,
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusManager = LocalFocusManager.current
-    val showLeadingIcon: @Composable () -> Unit = {
-        when (isSearchingProvider()) {
-            true -> {
-                CircularProgressIndicator(
-                    modifier = Modifier.size(25.dp)
-                )
-            }
 
-            false -> {
-                Icon(
-                    imageVector = Icons.Filled.Search,
-                    contentDescription = stringResource(R.string.search_field),
-                    tint = Color.Gray
-                )
-            }
-        }
-    }
-
-    val textFieldColors = TextFieldDefaults.textFieldColors(
-        focusedIndicatorColor = Color.Transparent,
-        unfocusedIndicatorColor = Color.Transparent,
-        containerColor = Color.DarkGray, textColor = Color.White,
-        cursorColor = Color.White
-    )
-
-    OutlinedTextField(
-        modifier = modifier,
-        value = searchTextProvider(),
-        onValueChange = onSearchTextChanged,
-        shape = RoundedCornerShape(20.dp),
-        placeholder = {
+    Box(modifier = Modifier.fillMaxWidth()) {
+        if (searchTextProvider().isEmpty())
             Text(
                 text = placeholderText,
                 color = Color.Gray,
-                fontSize = 20.sp,
-                textAlign = TextAlign.Center
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp),
             )
-        },
-        colors = textFieldColors,
-        leadingIcon = showLeadingIcon,
-        maxLines = 1,
-        singleLine = true,
-        keyboardActions = KeyboardActions(onDone = {
-            keyboardController?.hide()
-            focusManager.clearFocus()
-        })
-    )
+
+        BasicTextField(
+            value = searchTextProvider(),
+            onValueChange = onSearchTextChanged,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp),
+            singleLine = true,
+            textStyle = TextStyle(
+                color = MaterialTheme.colorScheme.inverseOnSurface,
+                fontSize = MaterialTheme.typography.titleMedium.fontSize
+            ),
+            cursorBrush = Brush.verticalGradient(
+                colors = listOf(
+                    Color.White,
+                    Color.White,
+                )
+            ),
+            maxLines = 1,
+            keyboardActions = KeyboardActions(onDone = {
+                keyboardController?.hide()
+                focusManager.clearFocus()
+            })
+        )
+    }
 }
